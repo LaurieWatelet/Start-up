82ea41cac6f31d81712737a4e3b275f326fc2fec
lapetitechevre <144000511+lapetitechevre@users.noreply.github.com>
2023-12-11 12:00:56 +0100

    changement

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="min-h-screen bg-blue-500">
+      <nav class="container mx-auto px-3 h-24 flex items-center mb-10">
+        <NuxtLink to="/">
+          <h1 class="text-5xl">Bienvenue chez GreenGenius : La Révolution Verte à Portée de Main</h1>
+        </NuxtLink>
+      </nav>
+      <div class="container mx-auto px-3">
+        <slot />
+      </div>
+    </div>
+  </template>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<script setup>
+const query = gql`
+  query PageAccueil {
+    page(where: {slug: "accueil"}) {
+    id
+    slug
+    titre
+    texte {
+      html
+    }
+  }
+}
+`;
+
+const contenuAccueil = ref();
+const { data } = await useAsyncQuery(query);
+console.log(data.value);
+contenuAccueil.value = data.value.page;
+</script>
+
+<template>
+<h2>
+    
+</h2>
+<div v-html="contenuAccueil.texte.html"></div>
+</template>
