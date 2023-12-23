2a322afe4925221bac1faad3e4cedf6d29691f67
lapetitechevre <144000511+lapetitechevre@users.noreply.github.com>
2023-12-23 11:08:09 +0100

    changement

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,7 @@
   <div class="min-h-screen bg-blue-500">
     <!--Début de la Nav-->
     <nav class="container mx-auto px-3 h-24 flex items-center mb-10">
-      <NuxtLink to="../produits/index">
+      <NuxtLink to="../produits/produits">
         <h1 class="text-5xl">Produits</h1>
       </NuxtLink>
 
--- pages/produits/produits.vue
+++ pages/produits/produits.vue
@@ -30,7 +30,7 @@ produits.value = data.value.produits;
 <template>
   <ul v-if="produits" class="flex flex-wrap justify-start">
     <li v-for="produit in produits" :key="produit.id" class="mx-7 my-4 shadow-xl">
-      <NuxtLink :to="`/produit/${produit.slug}`" class="text-center block">
+      <NuxtLink :to="`/${produit.slug}`" class="text-center block">
         <NuxtImg :src="produit.image.url" :alt="produit.nom" class="mx-auto mb-2" />
         <h2 class="text-center text-lg mb-2">{{ produit.nom }}</h2>
       </NuxtLink>
