d4b3bbd28ff5d6ea8412ed167c69302f8b46c3d5
lapetitechevre <144000511+lapetitechevre@users.noreply.github.com>
2023-12-23 01:21:34 +0100

    changement

--- pages/produits/[slug].vue
+++ pages/produits/[slug].vue
@@ -1,2 +1,48 @@
-<template>test</template>
-  
+<script setup>
+const query = gql`
+  query Produit($slug: String!) {
+    produit(where: { slug: $slug }) {
+      id
+      nom
+      slug
+      description
+      createdAt
+      publishedAt
+      updatedAt
+      stage
+      image {
+        url(
+          transformation: {
+            image: { resize: { fit: crop, height: 1024, width: 1024 } }
+            document: { output: { format: webp } }
+          }
+        )
+      }
+    }
+  }
+`;
+
+const produit = ref();
+
+const route = useRoute();
+const { data } = await useAsyncQuery(query, {
+  slug: route.params.slug,
+});
+
+console.log(data.value);
+produit.value = data.value.produit;
+
+</script>
+
+<!--Ce code controle la description quand on clique sur l'image-->
+
+<template class="">
+  <div v-if="produit" class="max-w-lg">
+    <NuxtImg :src="produit.image.url" :alt="produit.nom"/>
+    <h2 class="text-2xl text-center mb-3 mt-3">{{ produit.nom }}</h2>
+    <p class="description">{{ produit.description }}</p>
+  </div>
+  <div v-else>
+    <li>Loading...</li>
+  </div>
+</template>
--- pages/produits/produits.vue
+++ pages/produits/produits.vue
@@ -1,1 +1,42 @@
-<template>Test</template>
+<script setup>
+const query = gql`
+  query Produits {
+    produits(orderBy: id_ASC) {
+      createdAt
+      description
+      id
+      nom
+      publishedAt
+      slug
+      updatedAt
+      image {
+        url(
+          transformation: {
+            document: { output: { format: webp } }
+            image: { resize: { fit: crop, height: 256, width: 256 } }
+          }
+        )
+      }
+    }
+  }
+`;
+
+const produits = ref();
+const { data } = await useAsyncQuery(query);
+console.log(data.value);
+produits.value = data.value.produits;
+</script>
+<!--Ce code controle la description d'aperçu de l'image-->
+<template>
+  <ul v-if="produits" class="flex flex-wrap justify-start">
+    <li v-for="produit in produits" :key="produit.id" class="mx-7 my-4 shadow-xl">
+      <NuxtLink :to="`/produit/${produit.slug}`" class="text-center block">
+        <NuxtImg :src="produit.image.url" :alt="produit.nom" class="mx-auto mb-2" />
+        <h2 class="text-center text-lg mb-2">{{ produit.nom }}</h2>
+      </NuxtLink>
+    </li>
+  </ul>
+  <ul v-else>
+    <li>Loading...</li>
+  </ul>
+</template>
