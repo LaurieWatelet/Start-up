9994596e7d5fcb176d4b6b962ca1dcc3c527f4eb
lapetitechevre <144000511+lapetitechevre@users.noreply.github.com>
2023-12-25 00:37:01 +0100

    logo

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,30 +3,30 @@
   <!--Début de la div de naviguation-->
   <div class="min-h-screen bg-blue-500">
     <!--Début de la Nav-->
-    <nav class="container mx-auto px-3 h-24 flex items-center mb-10">
+    <nav class="container flex">
 
       <NuxtLink to="/">
-        <h1 class="text-5xl">Accueil</h1>
+        <h1 class="text-lg">Accueil</h1>
       </NuxtLink>
 
       <NuxtLink to="../produits/produits">
-        <h1 class="text-5xl">Produits</h1>
+        <h1 class="text-lg">Produits</h1>
       </NuxtLink>
 
       <NuxtLink to="/societe">
-        <h1 class="text-5xl">Société</h1>
+        <h1 class="text-lg">Société</h1>
       </NuxtLink>
 
       <NuxtLink to="../equipe/membre">
-        <h1 class="text-5xl">Equipe</h1>
+        <h1 class="text-lg">Équipe</h1>
       </NuxtLink>
 
       <NuxtLink to="/contact">
-        <h1 class="text-5xl">Contact</h1>
+        <h1 class="text-lg">Contact</h1>
       </NuxtLink>
 
       <NuxtLink to="../blog/article">
-        <h1 class="text-5xl">Blog</h1>
+        <h1 class="text-lg">Blog</h1>
       </NuxtLink>
     </nav>
     <!--Fin de la Nav-->
--- pages/index.vue
+++ pages/index.vue
@@ -17,9 +17,21 @@ const { data } = await useAsyncQuery(query);
 console.log(data.value);
 contenuAccueil.value = data.value.page;
 </script>
+
 <template>
   <div>
-    <h2>{{ contenuAccueil.titre }}</h2>
-    <div v-html="contenuAccueil.texte.html"></div>
+
+    <!-- Autres contenus de la page d'accueil -->
+
+    <!-- Encadré avec un CTA direction la page produits-->
+    <div class="bg-gray-200 p-8 my-8">
+      <h2 class="text-3xl font-bold mb-4">Découvrez nos produits et services</h2>
+      <p class="text-lg mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac libero quis justo dignissim tincidunt. Aenean vel massa id elit euismod imperdiet.</p>
+      <NuxtLink to="/produits/produits" class="bg-blue-500 text-white px-6 py-3 rounded bg-blue-700 transition duration-300 ease-in-out inline-block">
+        Voir nos produits
+      </NuxtLink>
+    </div>
+
+
   </div>
 </template>
