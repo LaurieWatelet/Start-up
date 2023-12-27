21eda2554b341c11e2d486f4c60670b460bfaaa6
lapetitechevre <144000511+lapetitechevre@users.noreply.github.com>
2023-12-27 15:50:20 +0100

    flex

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,34 +1,34 @@
 <template>
 
   <!--Container Div générale-->
-  <div class="border-2 border-rose-600 bg-[conic-gradient(at_right,_var(--tw-gradient-stops))] from-indigo-200 via-slate-600 to-indigo-200">
+  <div class="bg-gradient-to-r from-green-300 to-purple-400">
     <!--Naviguation container-->
     <nav class="container sticky bg-white flex items-center backdrop-filter backdrop-blur-lg bg-opacity-30 justify-center p-2 mx-auto text-black capitalize dark:text-gray-300">
 
       <img class="h-20" src="../pages/img/logo.png">
 
       <NuxtLink to="/">
-        <h1 class="text-lg border-b-2 dark:hover:text-gray-200 hover:border-green-500 mx-1.5 sm:mx-6">Accueil</h1>
+        <h1 class="text-base border-b-2 border-transparent text-black hover:border-green-500 mx-4 sm:mx-10">Accueil</h1>
       </NuxtLink>
 
       <NuxtLink to="../produits/produits">
-        <h1 class="text-lg border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-green-500 mx-1.5 sm:mx-6">Produits</h1>
+        <h1 class="text-base border-b-2 border-transparent text-black hover:border-green-500 mx-4 sm:mx-10">Produits</h1>
       </NuxtLink>
 
       <NuxtLink to="/societe">
-        <h1 class="text-lg border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-green-500 mx-1.5 sm:mx-6">Société</h1>
+        <h1 class="text-base border-b-2 border-transparent text-black hover:border-green-500 mx-4 sm:mx-10">Société</h1>
       </NuxtLink>
 
       <NuxtLink to="../equipe/membre">
-        <h1 class="text-lg border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-green-500 mx-1.5 sm:mx-6">Équipe</h1>
+        <h1 class="text-base border-b-2 border-transparent text-black hover:border-green-500 mx-4 sm:mx-10">Équipe</h1>
       </NuxtLink>
 
       <NuxtLink to="/contact">
-        <h1 class="text-lg border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-green-500 mx-1.5 sm:mx-6">Contact</h1>
+        <h1 class="text-base border-b-2 border-transparent text-black hover:border-green-500 mx-4 sm:mx-10">Contact</h1>
       </NuxtLink>
 
       <NuxtLink to="../blog/article">
-        <h1 class="text-lg border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-green-500 mx-1.5 sm:mx-6">Blog</h1>
+        <h1 class="text-base border-b-2 border-transparent text-black hover:border-green-500 mx-4 sm:mx-10">Blog</h1>
       </NuxtLink>
     </nav>
     <!--Fin de la Nav-->
--- pages/index.vue
+++ pages/index.vue
@@ -19,15 +19,21 @@ contenuAccueil.value = data.value.page;
 </script>
 
 <template>
-  <div>
 
-    <!-- Autres contenus de la page d'accueil -->
 
+
+    <div class="flex flex-row">
+      <h1 class="text-xl italic">"Innovation Intelligente, Solutions Simples, Votre Quotidien Réinventé..."</h1>
+      <img class="h-52" src="../pages/img/slogan.png">
+    </div>
+
+
+    <div class="flex flex-row border-2">
     <!-- Encadré avec un CTA direction la page produits-->
-    <div class="bg-purple-400 p-8 rounded max-w-md">
+    <div class="bg-purple-400 border-2 border-black p-8 rounded max-w-md">
       <h2 class="text-xl font-bold mb-4">Découvrez nos produits et services !</h2>
       <p class="text-base mb-8">Explorez l'avenir avec TecGiggle, là où l'innovation rencontre la simplicité. Plongez dans notre collection de gadgets et de services conçus pour rendre votre vie technologique plus excitante et sans tracas.</p>
-      <NuxtLink to="/produits/produits" class="bg-cyan-700 text-white px-6 py-3 rounded">
+      <NuxtLink to="/produits/produits" class="bg-cyan-700 text-white px-6 py-3 rsounded">
         Voir nos produits
       </NuxtLink>
     </div>
