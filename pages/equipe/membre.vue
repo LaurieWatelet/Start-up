bf58f73679ec31e1940291bc74b3778bac4756d0
lapetitechevre <144000511+lapetitechevre@users.noreply.github.com>
2023-12-27 12:04:40 +0100

    changement

--- pages/equipe/membre.vue
+++ pages/equipe/membre.vue
@@ -30,11 +30,12 @@ onMounted(async () => {
 <!--Ce code controle la description d'aperçu de l'image-->
 <template>
   <div>
+    <h1 class="text-4xl font-bold mb-8 text-center">Liste des Membres</h1>
     <ul v-if="membres" class="flex flex-wrap justify-start">
       <li v-for="membre in membres" :key="membre.id" class="mx-7 my-4 shadow-xl">
-        <div class="text-center block">
-          <NuxtImg :src="membre.image.url" :alt="membre.nom" class="mx-auto mb-2" />
-          <h2 class="text-center text-lg mb-2">{{ membre.nom }}</h2>
+        <div class="text-center block p-6">
+          <NuxtImg :src="membre.image.url" :alt="membre.nom" class="mx-auto mb-2 bg-purple-400" />
+          <h2 class="text-center text-lg">{{ membre.nom }}</h2>
           <p class="description text-md">{{ membre.description }}</p>
         </div>
       </li>
