1336cc5b2b27ad8c956e5785b131aefc5c2860ba
lapetitechevre <144000511+lapetitechevre@users.noreply.github.com>
2023-12-31 14:13:21 +0100

    style

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,5 @@
 <template>
+
   <!-- Contient toutes les modifications sur les pages design -->
   <div>
     <!-- Navigation container -->
@@ -35,17 +36,18 @@
     </div>
   </div>
 
-  <footer class="relative bg-blueGray-200 pt-8 pb-6">
+  <!-- Footer -->
+  <footer class="relative bg-sky-950 pt-8 pb-6">
   <div class="container mx-auto px-4">
     <div class="flex flex-wrap text-left lg:text-left">
       <div class="w-full lg:w-6/12 px-4">
-        <h4 class="text-3xl fonat-semibold text-blueGray-700">Let's keep in touch!</h4>
-        <h5 class="text-lg mt-0 mb-2 text-blueGray-600">
-          Find us on any of these platforms, we respond 1-2 business days.
+        <h4 class="text-3xl fonat-semibold text-white"> Restons en contact !</h4>
+        <h5 class="text-lg mt-0 mb-2 text-white">
+        Retrouvez-nous sur l'une de ces plateformes, nous répondons sous 1 à 2 jours ouvrables.
         </h5>
         <div class="mt-6 lg:mb-0 mb-6">
           <button class="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
-            <i class="fab fa-twitter"></i></button><button class="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
+            <i class="fa-brands fa-square-x-twitter"></i></button><button class="bg-white text-black shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
             <i class="fab fa-facebook-square"></i></button><button class="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
             <i class="fab fa-dribbble"></i></button><button class="bg-white text-blueGray-800 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
             <i class="fab fa-github"></i>
@@ -55,36 +57,36 @@
       <div class="w-full lg:w-6/12 px-4">
         <div class="flex flex-wrap items-top mb-6">
           <div class="w-full lg:w-4/12 px-4 ml-auto">
-            <span class="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Useful Links</span>
+            <span class="block uppercase text-white text-sm font-semibold mb-2">Liens utiles</span>
             <ul class="list-unstyled">
               <li>
-                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">About Us</a>
+                <a class="text-white hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">About Us</a>
               </li>
               <li>
-                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Blog</a>
+                <a class="text-white hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Blog</a>
               </li>
               <li>
-                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Github</a>
+                <a class="text-white hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Github</a>
               </li>
               <li>
-                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Free Products</a>
+                <a class="text-white hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Free Products</a>
               </li>
             </ul>
           </div>
           <div class="w-full lg:w-4/12 px-4">
-            <span class="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Other Resources</span>
+            <span class="block uppercase text-white text-sm font-semibold mb-2">Autres ressources</span>
             <ul class="list-unstyled">
               <li>
-                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">MIT License</a>
+                <a class="text-white hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">MIT License</a>
               </li>
               <li>
-                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="e">Terms &amp; Conditions</a>
+                <a class="text-white hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="e">Termes &amp; Conditions</a>
               </li>
               <li>
-                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Privacy Policy</a>
+                <a class="text-white hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Privacy Policy</a>
               </li>
               <li>
-                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Contact Us</a>
+                <a class="text-white hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Contact</a>
               </li>
             </ul>
           </div>
@@ -94,9 +96,9 @@
     <hr class="my-6 border-blueGray-300">
     <div class="flex flex-wrap items-center md:justify-between justify-center">
       <div class="w-full md:w-4/12 px-4 mx-auto text-center">
-        <div class="text-sm text-blueGray-500 font-semibold py-1">
-          Copyright © <span id="get-current-year">2021</span><a href=""></a> class="text-blueGray-500 hover:text-gray-800" target="_blank"> Notus JS by
-          <a href="" class="text-blueGray-500 hover:text-blueGray-800">Creative Tim</a>.
+        <div class="text-sm text-white font-semibold py-1">
+          Copyright © <span id="get-current-year">2023</span><a href=""></a>
+          <a href="" class="text-blueGray-500 hover:text-blueGray-800"></a>
         </div>
       </div>
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -23,11 +23,14 @@ contenuAccueil.value = data.value.page;
   <div class="flex flex-row pt-9 justify-evenly">
 
     <div class="flex flex-col m-0 p-0 justify-center max-w-lg">
-      <h1 class="text-3xl font-bold">ViruoPlay, transformez votre réalité, vivez votre rêve...</h1>
+      <h1 class="text-3xl mb-2 font-bold">ViruoPlay, transformez votre réalité, vivez votre rêve...</h1>
       <p class="text-lg">Découvrez un monde où la réalité se transforme en aventure, on vous ouvre les portes d'une expérience virtuelle captivante</p>
     </div>
-    <img class="h-64 rounded-2xl" src="@/pages/img/space.jpg" alt="Image de l'espace"/>
-
+    <img class="h-60 mt-6 rounded-2xl" src="@/pages/img/space.jpg" alt="Image de l'espace"/>
   </div>
   <!-- Fin du conteneur global -->
+
+
+
+  
 </template>
